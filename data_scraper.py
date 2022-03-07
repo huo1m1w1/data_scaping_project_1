c98aec8f806e5e8543c69fafb33705dbd4442e18
Mingwang Huo <90225549+huo1m1w1@users.noreply.github.com>
2022-03-07 22:55:45 +0000

    Update data_scraper.py

--- data_scraper.py
+++ data_scraper.py
@@ -7,26 +7,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 import uuid
-
-# import urllib
-
-# import urllib.request
-# import scrapy
-# import requests
-# from bs4 import BeautifulSoup
 import time
-
-# import datetime
-# from botocore.errorfactory import ClientError
-# from bs4 import BeautifulSoup
-# from urllib.request import Request, urlopen
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
-# from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.service import Service
-
-# import json
-# from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import pandas as pd
 
 
